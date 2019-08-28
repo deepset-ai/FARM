e49264d2896865c7dce6197d3294367dcaab631a
Malte Pietsch <malte.pietsch@deepset.ai>
2019-08-28 18:46:51 +0200

    Add InferenceProcessor (#72)
    Adds a lightweight processor for inference that is particularly useful if you used a complex or custom Processor at training time, but you only want to use the model as a text embedder at inference time.

--- farm/infer.py
+++ farm/infer.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import logging
 
 from torch.utils.data.sampler import SequentialSampler
 
@@ -7,8 +8,11 @@ from farm.data_handler.dataloader import NamedDataLoader
 from farm.modeling.adaptive_model import AdaptiveModel
 
 from farm.utils import initialize_device_settings
-from farm.data_handler.processor import Processor
+from farm.data_handler.processor import Processor, InferenceProcessor
 from farm.utils import set_all_seeds
+
+
+logger = logging.getLogger(__name__)
 
 
 class Inferencer:
@@ -24,7 +28,7 @@ class Inferencer:
            {"text": "Schartau sagte dem Tagesspiegel, dass Fischer ein Idiot sei"},
            {"text": "Martin Müller spielt Handball in Berlin"},
        ]
-       model = Inferencer(your_model_dir)
+       model = Inferencer.load(your_model_dir)
        model.run_inference(dicts=basic_texts)
        # LM embeddings
        model.extract_vectors(dicts=basic_texts)
@@ -69,15 +73,19 @@ class Inferencer:
         set_all_seeds(42, n_gpu)
 
     @classmethod
-    def load(cls, load_dir, batch_size=4, gpu=False):
+    def load(cls, load_dir, batch_size=4, gpu=False, embedder_only=False):
         """
         Initializes inferencer from directory with saved model.
+
         :param load_dir: Directory where the saved model is located.
         :type load_dir: str
         :param batch_size: Number of samples computed once per batch
         :type batch_size: int
         :param gpu: If GPU shall be used
         :type gpu: bool
+        :param embedder_only: If true, a faster processor (InferenceProcessor) is loaded. This should only be used
+        for extracting embeddings (no downstream predictions).
+        :type embedder_only: bool
         :return: An instance of the Inferencer.
         """
 
@@ -86,23 +94,30 @@ class Inferencer:
         )
 
         model = AdaptiveModel.load(load_dir, device)
-        processor = Processor.load_from_dir(load_dir)
+        if embedder_only:
+            processor = InferenceProcessor.load_from_dir(load_dir)
+        else:
+            processor = Processor.load_from_dir(load_dir)
+
         name = os.path.basename(load_dir)
         return cls(model, processor, batch_size=batch_size, gpu=gpu, name=name)
 
     def run_inference(self, dicts):
         """
         Runs down-stream inference using the prediction head.
+
         :param dicts: Samples to run inference on provided as a list of dicts. One dict per sample.
         :type dicst: [dict]
         :return: dict of predictions
 
         """
         if self.prediction_type == "embedder":
-            raise TypeError("You have called run_inference for a model without any prediction head! "
-                            "If you want to: "
-                            "a) ... extract vectors from the language model: call `Inferencer.extract_vectors(...)`"
-                            f"b) ... run inference on a downstream task: make sure your model path {self.name} contains a saved prediction head")
+            raise TypeError(
+                "You have called run_inference for a model without any prediction head! "
+                "If you want to: "
+                "a) ... extract vectors from the language model: call `Inferencer.extract_vectors(...)`"
+                f"b) ... run inference on a downstream task: make sure your model path {self.name} contains a saved prediction head"
+            )
         dataset, tensor_names = self.processor.dataset_from_dicts(dicts)
         samples = []
         for dict in dicts:
@@ -116,9 +131,9 @@ class Inferencer:
         )
 
         preds_all = []
-        for i,batch in enumerate(data_loader):
+        for i, batch in enumerate(data_loader):
             batch = {key: batch[key].to(self.device) for key in batch}
-            batch_samples = samples[i*self.batch_size:(i+1)*self.batch_size]
+            batch_samples = samples[i * self.batch_size : (i + 1) * self.batch_size]
             with torch.no_grad():
                 logits = self.model.forward(**batch)
                 preds = self.model.formatted_preds(
@@ -126,21 +141,28 @@ class Inferencer:
                     label_maps=self.processor.label_maps,
                     samples=batch_samples,
                     tokenizer=self.processor.tokenizer,
-                    **batch
+                    **batch,
                 )
                 preds_all += preds
 
         return preds_all
 
-    def extract_vectors(self, dicts, extraction_strategy="pooled"):
+    def extract_vectors(
+        self, dicts, extraction_strategy="cls_token", extraction_layer=-1
+    ):
         """
         Converts a text into vector(s) using the language model only (no prediction head involved).
+
         :param dicts: Samples to run inference on provided as a list of dicts. One dict per sample.
         :type dicts: [dict]
-        :param extraction_strategy: Strategy to extract vectors. Choices: 'pooled' (sentence vector), 'per_token' (individual token vectors)
+        :param extraction_strategy: Strategy to extract vectors. Choices: 'cls_token' (sentence vector),
+        'reduce_mean' (sentence vector), reduce_max (sentence vector), 'per_token' (individual token vectors)
         :type extraction_strategy: str
+        :param extraction_layer: number of layer from which the embeddings shall be extracted. Default: -1 (very last layer).
+        :type: int
         :return: dict of predictions
         """
+
         dataset, tensor_names = self.processor.dataset_from_dicts(dicts)
         samples = []
         for dict in dicts:
@@ -154,16 +176,55 @@ class Inferencer:
         )
 
         preds_all = []
-        for i,batch in enumerate(data_loader):
+        for i, batch in enumerate(data_loader):
             batch = {key: batch[key].to(self.device) for key in batch}
-            batch_samples = samples[i*self.batch_size:(i+1)*self.batch_size]
+            batch_samples = samples[i * self.batch_size : (i + 1) * self.batch_size]
             with torch.no_grad():
                 preds = self.model.language_model.formatted_preds(
                     extraction_strategy=extraction_strategy,
                     samples=batch_samples,
                     tokenizer=self.processor.tokenizer,
-                    **batch
+                    extraction_layer=extraction_layer,
+                    **batch,
                 )
                 preds_all += preds
+
+        return preds_all
+
+
+class FasttextInferencer:
+    def __init__(self, model, name=None):
+        self.model = model
+        self.name = name if name != None else f"anonymous-fasttext"
+        self.prediction_type = "embedder"
+
+    @classmethod
+    def load(cls, load_dir, batch_size=4, gpu=False, embedder_only=True):
+        import fasttext
+        if os.path.isfile(load_dir):
+            return cls(model=fasttext.load_model(load_dir))
+        else:
+            logger.error(f"Fasttext model file does not exist at: {load_dir}")
+
+    def extract_vectors(self, dicts, extraction_strategy="reduce_mean"):
+        """
+        Converts a text into vector(s) using the language model only (no prediction head involved).
+
+        :param dicts: Samples to run inference on provided as a list of dicts. One dict per sample.
+        :type dicts: [dict]
+        :param extraction_strategy: Strategy to extract vectors. Choices: 'reduce_mean' (mean sentence vector), 'reduce_max' (max per embedding dim), 'CLS'
+        :type extraction_strategy: str
+        :return: dict of predictions
+        """
+
+        preds_all = []
+        for d in dicts:
+            pred = {}
+            pred["context"] = d["text"]
+            if extraction_strategy == "reduce_mean":
+                pred["vec"] = self.model.get_sentence_vector(d["text"])
+            else:
+                raise NotImplementedError
+            preds_all.append(pred)
 
         return preds_all
