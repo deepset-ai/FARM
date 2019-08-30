941bfa4ab46c31f09674a98735daafbdcf6d949c
Malte Pietsch <malte.pietsch@deepset.ai>
2019-08-30 10:05:31 +0200

    Add max number of docs to read from file (#73)

--- farm/data_handler/processor.py
+++ farm/data_handler/processor.py
@@ -664,6 +664,7 @@ class BertStyleLMProcessor(Processor):
         dev_filename="dev.txt",
         test_filename="test.txt",
         dev_split=0.0,
+        max_docs=None,
         **kwargs,
     ):
         # General Processor attributes
@@ -675,6 +676,7 @@ class BertStyleLMProcessor(Processor):
 
         # Custom attributes
         self.delimiter = ""
+        self.max_docs = max_docs
 
         super(BertStyleLMProcessor, self).__init__(
             tokenizer=tokenizer,
@@ -692,7 +694,7 @@ class BertStyleLMProcessor(Processor):
         )
 
     def _file_to_dicts(self, file: str) -> list:
-        dicts = read_docs_from_txt(filename=file, delimiter=self.delimiter)
+        dicts = read_docs_from_txt(filename=file, delimiter=self.delimiter, max_docs=self.max_docs)
         return dicts
 
     @classmethod
