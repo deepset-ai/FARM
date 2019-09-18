7e8a77b780912fd331a0e57bfb35701fe2c568be
Tanay Soni <tanaysoni12@gmail.com>
2019-09-18 10:30:52 +0200

    Fix for file descriptor leak when using multiprocessing with pytorch

--- farm/__init__.py
+++ farm/__init__.py
@@ -1,12 +1,11 @@
 import logging
+import torch
+
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
-    level=logging.INFO)
+    level=logging.INFO,
+)
 
-
-#fixing a environment issue on increasing number file descriptors
-#fix accoring to this pytorch issue: https://github.com/pytorch/pytorch/issues/973#issuecomment-346405667
-import resource
-rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (1_000_000, rlimit[1]))
+# fix for a file descriptor issue when using multiprocessing: https://github.com/pytorch/pytorch/issues/973
+torch.multiprocessing.set_sharing_strategy("file_system")
