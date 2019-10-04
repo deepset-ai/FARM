93c93dd3ab24519738a9d4261c18b41bb40874a7
Malte Pietsch <malte.pietsch@deepset.ai>
2019-10-04 17:49:53 +0200

    add more tests. fix custom vocab for bert-base-cased (has no unused0 token)

---
+++ test/test_processor_saving_loading.py
@@ -0,0 +1,39 @@
+import logging
+from farm.data_handler.processor import TextClassificationProcessor
+from farm.modeling.tokenization import BertTokenizer
+from farm.utils import set_all_seeds
+import torch
+
+def test_processor_saving_loading(caplog):
+    caplog.set_level(logging.CRITICAL)
+    set_all_seeds(seed=42)
+    lang_model = "bert-base-cased"
+
+    tokenizer = BertTokenizer.from_pretrained(
+        pretrained_model_name_or_path=lang_model, do_lower_case=False, never_split_chars=["-", "_"]
+    )
+
+    processor = TextClassificationProcessor(tokenizer=tokenizer,
+                                            max_seq_len=128,
+                                            data_dir="samples/doc_class",
+                                            train_filename="train-sample.tsv",
+                                            dev_filename=None,
+                                            test_filename=None,
+                                            dev_split=0.1,
+                                            columns=["text", "label", "unused"],
+                                            label_list=["OTHER", "OFFENSE"],
+                                            metrics=["f1_macro"]
+                                            )
+    dicts = processor._file_to_dicts(file="samples/doc_class/train-sample.tsv")
+    data, tensor_names = processor.dataset_from_dicts(dicts)
+
+    save_dir = "testsave/processor"
+    processor.save(save_dir)
+
+    processor = processor.load_from_dir(save_dir)
+    dicts = processor._file_to_dicts(file="samples/doc_class/train-sample.tsv")
+    data_loaded, tensor_names_loaded = processor.dataset_from_dicts(dicts)
+
+    assert tensor_names == tensor_names_loaded
+    for i in range(len(data.tensors)):
+        assert torch.all(torch.eq(data.tensors[i], data_loaded.tensors[i]))
---
+++ test/test_tokenization.py
@@ -0,0 +1,70 @@
+import logging
+from farm.modeling.tokenization import BertTokenizer, tokenize_with_metadata
+
+def test_bert_tokenizer(caplog):
+    caplog.set_level(logging.CRITICAL)
+
+    lang_model = "bert-base-cased"
+
+    tokenizer = BertTokenizer.from_pretrained(
+        pretrained_model_name_or_path=lang_model,
+        do_lower_case=False)
+    #tokenizer.add_special_tokens()
+
+    basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
+
+    # original tokenizer from transformer repo
+    tokenized = tokenizer.tokenize(basic_text)
+    assert tokenized == ['Some', 'Text', 'with', 'never', '##see', '##nto', '##ken', '##s', 'plus', '!', '215', '?', '#', '.', 'and', 'a', 'combined', '-', 'token', '_', 'with', '/', 'ch', '##ars']
+
+    # ours with metadata
+    tokenized_meta = tokenize_with_metadata(text=basic_text, tokenizer=tokenizer,max_seq_len=64)
+    assert tokenized_meta["tokens"] == tokenized
+    assert tokenized_meta["offsets"] == [0, 5, 10, 15, 20, 23, 26, 29, 31, 36, 37, 40, 41, 42, 44, 48, 50, 58, 59, 64, 65, 69, 70, 72]
+    assert tokenized_meta["start_of_word"] == [True, True, True, True, False, False, False, False, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False]
+
+
+def test_bert_custom_vocab(caplog):
+    caplog.set_level(logging.CRITICAL)
+
+    lang_model = "bert-base-cased"
+
+    tokenizer = BertTokenizer.from_pretrained(
+        pretrained_model_name_or_path=lang_model,
+        do_lower_case=False)
+    tokenizer.add_custom_vocab("samples/tokenizer/custom_vocab.txt")
+    #tokenizer.add_special_tokens()
+
+    basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
+
+    # original tokenizer from transformer repo
+    tokenized = tokenizer.tokenize(basic_text)
+    assert tokenized == ['Some', 'Text', 'with', 'neverseentokens', 'plus', '!', '215', '?', '#', '.', 'and', 'a', 'combined', '-', 'token', '_', 'with', '/', 'ch', '##ars']
+
+    # ours with metadata
+    tokenized_meta = tokenize_with_metadata(text=basic_text, tokenizer=tokenizer,max_seq_len=64)
+    assert tokenized_meta["tokens"] == tokenized
+    assert tokenized_meta["offsets"] == [0, 5, 10, 15, 31, 36, 37, 40, 41, 42, 44, 48, 50, 58, 59, 64, 65, 69, 70, 72]
+    assert tokenized_meta["start_of_word"] == [True, True, True, True, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False]
+
+def test_bert_never_split_chars(caplog):
+    caplog.set_level(logging.CRITICAL)
+
+    tokenizer = BertTokenizer("samples/tokenizer/bert-base-cased-vocab.txt", do_lower_case=False, never_split_chars=["-","_","/"])
+    tokenizer.add_custom_vocab("samples/tokenizer/custom_vocab.txt")
+    #tokenizer.add_special_tokens()
+
+    basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
+
+    # original tokenizer from transformer repo
+    tokenized = tokenizer.tokenize(basic_text)
+    assert tokenized == ['Some', 'Text', 'with', 'neverseentokens', 'plus', '!', '215', '?', '#', '.', 'and', 'a', 'combined', '##-', '##tok', '##en', '##_', '##with', '##/', '##cha', '##rs']
+
+    # ours with metadata
+    tokenized_meta = tokenize_with_metadata(text=basic_text, tokenizer=tokenizer,max_seq_len=64)
+    assert tokenized_meta["tokens"] == tokenized
+    assert tokenized_meta["offsets"] ==  [0, 5, 10, 15, 31, 36, 37, 40, 41, 42, 44, 48, 50, 58, 59, 62, 64, 65, 69, 70, 73]
+    assert tokenized_meta["start_of_word"] == [True, True, True, True, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False, False]
+
+if(__name__=="__main__"):
+    test_bert_tokenizer()
