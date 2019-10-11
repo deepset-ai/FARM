222ce571f44bcb8c7536137274271d07712a4af4
Malte Pietsch <malte.pietsch@deepset.ai>
2019-10-11 15:56:57 +0200

    tokenize_with_metadata working with roberta,xlnet,bert incl offsets

--- farm/modeling/tokenization.py
+++ farm/modeling/tokenization.py
@@ -20,149 +20,151 @@ import logging
 from io import open
 import os
 import unicodedata
+import re
 
 from transformers.tokenization_bert import BertTokenizer, WordpieceTokenizer, BasicTokenizer, load_vocab
 from transformers.tokenization_roberta import RobertaTokenizer
+from transformers.tokenization_xlnet import XLNetTokenizer
 
 logger = logging.getLogger(__name__)
 
 
-class BasicTokenizer(BasicTokenizer):
-    def __init__(self, do_lower_case=True, never_split=None, never_split_chars=None, tokenize_chinese_chars=True):
-        """ Constructs a BasicTokenizer.
-
-        Args:
-            **do_lower_case**: Whether to lower case the input.
-            **never_split**: (`optional`) list of str
-                Kept for backward compatibility purposes.
-                Now implemented directly at the base class level (see :func:`PreTrainedTokenizer.tokenize`)
-                List of token not to split.
-            **tokenize_chinese_chars**: (`optional`) boolean (default True)
-                Whether to tokenize Chinese characters.
-                This should likely be desactivated for Japanese:
-                see: https://github.com/huggingface/pytorch-pretrained-BERT/issues/328
-        """
-        if never_split is None:
-            never_split = []
-        self.do_lower_case = do_lower_case
-        self.never_split = never_split
-        self.tokenize_chinese_chars = tokenize_chinese_chars
-        self.never_split_chars = never_split_chars
-
-    def _run_split_on_punc(self, text):
-        """Splits punctuation on a piece of text."""
-        if text in self.never_split:
-            return [text]
-        chars = list(text)
-        i = 0
-        start_new_word = True
-        output = []
-        while i < len(chars):
-            char = chars[i]
-            if _is_punctuation(char, excluded_chars=self.never_split_chars):
-                output.append([char])
-                start_new_word = True
-            else:
-                if start_new_word:
-                    output.append([])
-                start_new_word = False
-                output[-1].append(char)
-            i += 1
-
-        return ["".join(x) for x in output]
-
-
-class BertTokenizer(BertTokenizer):
-
-    def __init__(self, vocab_file, do_lower_case=True, do_basic_tokenize=True, never_split=None, never_split_chars=None,
-                 unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
-                 mask_token="[MASK]", tokenize_chinese_chars=True, **kwargs):
-        """Constructs a BertTokenizer.
-
-        Args:
-            **vocab_file**: Path to a one-wordpiece-per-line vocabulary file
-            **do_lower_case**: (`optional`) boolean (default True)
-                Whether to lower case the input
-                Only has an effect when do_basic_tokenize=True
-            **do_basic_tokenize**: (`optional`) boolean (default True)
-                Whether to do basic tokenization before wordpiece.
-            **never_split**: (`optional`) list of string
-                List of tokens which will never be split during tokenization.
-                Only has an effect when do_basic_tokenize=True
-            **tokenize_chinese_chars**: (`optional`) boolean (default True)
-                Whether to tokenize Chinese characters.
-                This should likely be desactivated for Japanese:
-                see: https://github.com/huggingface/pytorch-pretrained-BERT/issues/328
-        """
-        super(BertTokenizer, self).__init__(vocab_file, do_lower_case=do_lower_case, do_basic_tokenize=True, never_split=never_split, never_split_chars=never_split_chars,
-                 unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
-                 mask_token="[MASK]", tokenize_chinese_chars=tokenize_chinese_chars, **kwargs)
-        self.subword_identifier = "##"
-        if not os.path.isfile(vocab_file):
-            raise ValueError(
-                "Can't find a vocabulary file at path '{}'. To load the vocabulary from a Google pretrained "
-                "model use `tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)`".format(vocab_file))
-        self.vocab = load_vocab(vocab_file)
-        self.ids_to_tokens = collections.OrderedDict(
-            [(ids, tok) for tok, ids in self.vocab.items()])
-        self.do_basic_tokenize = do_basic_tokenize
-        if do_basic_tokenize:
-            self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case,
-                                                  never_split=never_split,
-                                                  never_split_chars=never_split_chars,
-                                                  tokenize_chinese_chars=tokenize_chinese_chars)
-        self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab, unk_token=self.unk_token)
-        assert len(self.vocab) > 0
-        assert self.wordpiece_tokenizer is not None
-
-
-    def add_custom_vocab(self, custom_vocab_file):
-        self.vocab = self._load_custom_vocab(custom_vocab_file)
-        self.ids_to_tokens = collections.OrderedDict(
-            [(ids, tok) for tok, ids in self.vocab.items()])
-        self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab, unk_token=self.unk_token)
-
-    def _load_custom_vocab(self, custom_vocab_file):
-        custom_vocab = {}
-        unique_custom_tokens = set()
-        idx = 0
-        num_dropped = 0
-        with open(custom_vocab_file, "r", encoding="utf-8") as reader:
-            while True:
-                token = reader.readline().strip()
-                if not token:
-                    break
-
-                if token not in unique_custom_tokens:
-                    if token not in self.vocab.keys():
-                        key = "[unused{}]".format(idx)
-                        custom_vocab[key] = token
-                        idx += 1
-                        unique_custom_tokens.add(token)
-                    else:
-                        num_dropped += 1
-                        logger.info("Dropped custom token (already in original vocab): {}".format(token))
-                else:
-                    logger.info("Dropped custom token (duplicate): {}".format(token))
-        # merge vocabs
-        update_count = 0
-        updated_vocab = []
-        for k,v in self.vocab.items():
-            if k in custom_vocab.keys():
-                updated_vocab.append((custom_vocab[k], v))
-                update_count += 1
-            else:
-                updated_vocab.append((k, v))
-        self.vocab = collections.OrderedDict(updated_vocab)
-
-        if update_count < len(custom_vocab):
-            logger.warning("Updated vocabulary only with {} out of {} tokens from supplied custom vocabulary. The original vocab might not have enough unused tokens.".format(update_count, len(custom_vocab)))
-        else:
-            logger.info("Updated vocabulary with {} out of {} tokens from custom vocabulary.".format(update_count, len(custom_vocab)))
-        if(num_dropped > 0):
-            logger.info(f"Dropped {num_dropped} items from custom vocab, because they were already contained in original vocab.")
-
-        return self.vocab
+# class BasicTokenizer(BasicTokenizer):
+#     def __init__(self, do_lower_case=True, never_split=None, never_split_chars=None, tokenize_chinese_chars=True):
+#         """ Constructs a BasicTokenizer.
+#
+#         Args:
+#             **do_lower_case**: Whether to lower case the input.
+#             **never_split**: (`optional`) list of str
+#                 Kept for backward compatibility purposes.
+#                 Now implemented directly at the base class level (see :func:`PreTrainedTokenizer.tokenize`)
+#                 List of token not to split.
+#             **tokenize_chinese_chars**: (`optional`) boolean (default True)
+#                 Whether to tokenize Chinese characters.
+#                 This should likely be desactivated for Japanese:
+#                 see: https://github.com/huggingface/pytorch-pretrained-BERT/issues/328
+#         """
+#         if never_split is None:
+#             never_split = []
+#         self.do_lower_case = do_lower_case
+#         self.never_split = never_split
+#         self.tokenize_chinese_chars = tokenize_chinese_chars
+#         self.never_split_chars = never_split_chars
+#
+#     def _run_split_on_punc(self, text):
+#         """Splits punctuation on a piece of text."""
+#         if text in self.never_split:
+#             return [text]
+#         chars = list(text)
+#         i = 0
+#         start_new_word = True
+#         output = []
+#         while i < len(chars):
+#             char = chars[i]
+#             if _is_punctuation(char, excluded_chars=self.never_split_chars):
+#                 output.append([char])
+#                 start_new_word = True
+#             else:
+#                 if start_new_word:
+#                     output.append([])
+#                 start_new_word = False
+#                 output[-1].append(char)
+#             i += 1
+#
+#         return ["".join(x) for x in output]
+#
+#
+# class BertTokenizer(BertTokenizer):
+#
+#     def __init__(self, vocab_file, do_lower_case=True, do_basic_tokenize=True, never_split=None, never_split_chars=None,
+#                  unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
+#                  mask_token="[MASK]", tokenize_chinese_chars=True, **kwargs):
+#         """Constructs a BertTokenizer.
+#
+#         Args:
+#             **vocab_file**: Path to a one-wordpiece-per-line vocabulary file
+#             **do_lower_case**: (`optional`) boolean (default True)
+#                 Whether to lower case the input
+#                 Only has an effect when do_basic_tokenize=True
+#             **do_basic_tokenize**: (`optional`) boolean (default True)
+#                 Whether to do basic tokenization before wordpiece.
+#             **never_split**: (`optional`) list of string
+#                 List of tokens which will never be split during tokenization.
+#                 Only has an effect when do_basic_tokenize=True
+#             **tokenize_chinese_chars**: (`optional`) boolean (default True)
+#                 Whether to tokenize Chinese characters.
+#                 This should likely be desactivated for Japanese:
+#                 see: https://github.com/huggingface/pytorch-pretrained-BERT/issues/328
+#         """
+#         super(BertTokenizer, self).__init__(vocab_file, do_lower_case=do_lower_case, do_basic_tokenize=True, never_split=never_split, never_split_chars=never_split_chars,
+#                  unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
+#                  mask_token="[MASK]", tokenize_chinese_chars=tokenize_chinese_chars, **kwargs)
+#         self.subword_identifier = "##"
+#         if not os.path.isfile(vocab_file):
+#             raise ValueError(
+#                 "Can't find a vocabulary file at path '{}'. To load the vocabulary from a Google pretrained "
+#                 "model use `tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)`".format(vocab_file))
+#         self.vocab = load_vocab(vocab_file)
+#         self.ids_to_tokens = collections.OrderedDict(
+#             [(ids, tok) for tok, ids in self.vocab.items()])
+#         self.do_basic_tokenize = do_basic_tokenize
+#         if do_basic_tokenize:
+#             self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case,
+#                                                   never_split=never_split,
+#                                                   never_split_chars=never_split_chars,
+#                                                   tokenize_chinese_chars=tokenize_chinese_chars)
+#         self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab, unk_token=self.unk_token)
+#         assert len(self.vocab) > 0
+#         assert self.wordpiece_tokenizer is not None
+#
+#
+#     def add_custom_vocab(self, custom_vocab_file):
+#         self.vocab = self._load_custom_vocab(custom_vocab_file)
+#         self.ids_to_tokens = collections.OrderedDict(
+#             [(ids, tok) for tok, ids in self.vocab.items()])
+#         self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab, unk_token=self.unk_token)
+#
+#     def _load_custom_vocab(self, custom_vocab_file):
+#         custom_vocab = {}
+#         unique_custom_tokens = set()
+#         idx = 0
+#         num_dropped = 0
+#         with open(custom_vocab_file, "r", encoding="utf-8") as reader:
+#             while True:
+#                 token = reader.readline().strip()
+#                 if not token:
+#                     break
+#
+#                 if token not in unique_custom_tokens:
+#                     if token not in self.vocab.keys():
+#                         key = "[unused{}]".format(idx)
+#                         custom_vocab[key] = token
+#                         idx += 1
+#                         unique_custom_tokens.add(token)
+#                     else:
+#                         num_dropped += 1
+#                         logger.info("Dropped custom token (already in original vocab): {}".format(token))
+#                 else:
+#                     logger.info("Dropped custom token (duplicate): {}".format(token))
+#         # merge vocabs
+#         update_count = 0
+#         updated_vocab = []
+#         for k,v in self.vocab.items():
+#             if k in custom_vocab.keys():
+#                 updated_vocab.append((custom_vocab[k], v))
+#                 update_count += 1
+#             else:
+#                 updated_vocab.append((k, v))
+#         self.vocab = collections.OrderedDict(updated_vocab)
+#
+#         if update_count < len(custom_vocab):
+#             logger.warning("Updated vocabulary only with {} out of {} tokens from supplied custom vocabulary. The original vocab might not have enough unused tokens.".format(update_count, len(custom_vocab)))
+#         else:
+#             logger.info("Updated vocabulary with {} out of {} tokens from custom vocabulary.".format(update_count, len(custom_vocab)))
+#         if(num_dropped > 0):
+#             logger.info(f"Dropped {num_dropped} items from custom vocab, because they were already contained in original vocab.")
+#
+#         return self.vocab
 
 
 
@@ -197,7 +199,10 @@ def _words_to_tokens(words, word_offsets, tokenizer, max_seq_len):
         if len(tokens) == 0:
             tokens_word = tokenizer.tokenize(w)
         else:
-            tokens_word = tokenizer.tokenize(w, add_prefix_space=True)
+            try:
+                tokens_word = tokenizer.tokenize(w, add_prefix_space=True)
+            except TypeError:
+                tokens_word = tokenizer.tokenize(w)
 
         # Sometimes the tokenizer returns no tokens
         if len(tokens_word) == 0:
@@ -208,7 +213,10 @@ def _words_to_tokens(words, word_offsets, tokenizer, max_seq_len):
         first_tok = True
         for tok in tokens_word:
             token_offsets.append(w_off)
-            w_off += len(tok.replace("##", ""))
+            # Depending on the tokenizer type special chars are added to distinguish tokens with preceeding
+            # whitespace (=> "start of a word"). We need to get rid of these to calculate the original length of the token
+            orig_tok = re.sub(r"^(##|Ġ|▁)", "", tok)
+            w_off += len(orig_tok)
             if first_tok:
                 start_of_word.append(True)
                 first_tok = False
--- test/test_tokenization.py
+++ test/test_tokenization.py
@@ -1,29 +1,30 @@
 import logging
-from farm.modeling.tokenization import BertTokenizer,RobertaTokenizer, tokenize_with_metadata
+from farm.modeling.tokenization import BertTokenizer, RobertaTokenizer, XLNetTokenizer, tokenize_with_metadata
+import re
+
+# deprecated:
+# def test_bert_a_never_split_chars(caplog):
+#     caplog.set_level(logging.CRITICAL)
+#     lang_model = "bert-base-cased"
+#
+#     tokenizer = BertTokenizer.from_pretrained(
+#         pretrained_model_name_or_path=lang_model,
+#         do_lower_case=False,
+#         never_split_chars=["-","_","/"])
+#
+#     basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
+#
+#     # original tokenizer from transformer repo
+#     tokenized = tokenizer.tokenize(basic_text)
+#     assert tokenized == ['Some', 'Text', 'with', 'never', '##see', '##nto', '##ken', '##s', 'plus', '!', '215', '?', '#','.', 'and', 'a', 'combined', '##-', '##tok', '##en', '##_', '##with', '##/', '##cha', '##rs']
+#     # ours with metadata
+#     tokenized_meta = tokenize_with_metadata(text=basic_text, tokenizer=tokenizer,max_seq_len=64)
+#     assert tokenized_meta["tokens"] == tokenized
+#     assert tokenized_meta["offsets"] == [0, 5, 10, 15, 31, 36, 37, 40, 41, 42, 44, 48, 50, 58, 59, 62, 64, 65, 69, 70, 73]
+#     assert tokenized_meta["start_of_word"] == [True, True, True, True, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False, False]
 
 
-def test_bert_a_never_split_chars(caplog):
-    caplog.set_level(logging.CRITICAL)
-    lang_model = "bert-base-cased"
-
-    tokenizer = BertTokenizer.from_pretrained(
-        pretrained_model_name_or_path=lang_model,
-        do_lower_case=False,
-        never_split_chars=["-","_","/"])
-
-    basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
-
-    # original tokenizer from transformer repo
-    tokenized = tokenizer.tokenize(basic_text)
-    assert tokenized == ['Some', 'Text', 'with', 'never', '##see', '##nto', '##ken', '##s', 'plus', '!', '215', '?', '#','.', 'and', 'a', 'combined', '##-', '##tok', '##en', '##_', '##with', '##/', '##cha', '##rs']
-    # ours with metadata
-    tokenized_meta = tokenize_with_metadata(text=basic_text, tokenizer=tokenizer,max_seq_len=64)
-    assert tokenized_meta["tokens"] == tokenized
-    assert tokenized_meta["offsets"] == [0, 5, 10, 15, 31, 36, 37, 40, 41, 42, 44, 48, 50, 58, 59, 62, 64, 65, 69, 70, 73]
-    assert tokenized_meta["start_of_word"] == [True, True, True, True, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False, False]
-
-
-def test_bert_tokenizer(caplog):
+def test_bert_tokenizer_all_meta(caplog):
     caplog.set_level(logging.CRITICAL)
 
     lang_model = "bert-base-cased"
@@ -60,7 +61,9 @@ def test_all_tokenizer_on_special_cases(caplog):
     roberta_tokenizer = RobertaTokenizer.from_pretrained(
         pretrained_model_name_or_path="roberta-base")
 
-    tokenizers = [roberta_tokenizer, bert_tokenizer]
+    xlnet_tokenizer = XLNetTokenizer.from_pretrained(pretrained_model_name_or_path="xlnet-base-cased")
+
+    tokenizers = [roberta_tokenizer, bert_tokenizer, xlnet_tokenizer]
 
     texts = [
      "This is a sentence",
@@ -76,18 +79,24 @@ def test_all_tokenizer_on_special_cases(caplog):
 
     for tokenizer in tokenizers:
         for text in texts:
+            # Important: we don't assume to preserve whitespaces after tokenization.
+            # This means: \t, \n " " etc will all resolve to a single " ".
+            # This doesn't make a difference for BERT + XLNet but it does for roBERTa
+
             # 1. original tokenize function from transformer repo on full sentence
-            text = ' '.join(text.split())
+            standardized_whitespace_text = ' '.join(text.split()) # remove multiple whitespaces
 
-            tokenized = tokenizer.tokenize(text)
+            tokenized = tokenizer.tokenize(standardized_whitespace_text)
             tokenized_by_word = []
-
             # 2. original tokenize function from transformer repo on "whitespace tokenized words"
             for i, tok in enumerate(text.split(" ")):
                 if i == 0:
                     tokenized_tok = tokenizer.tokenize(tok)
                 else:
-                    tokenized_tok = tokenizer.tokenize(tok, add_prefix_space=True)
+                    try:
+                        tokenized_tok = tokenizer.tokenize(tok, add_prefix_space=True)
+                    except TypeError:
+                        tokenized_tok = tokenizer.tokenize(tok)
                 tokenized_by_word.extend(tokenized_tok)
             assert tokenized == tokenized_by_word
 
@@ -103,11 +112,10 @@ def test_all_tokenizer_on_special_cases(caplog):
                 continue
             for tok, offset in zip(tokenized_meta["tokens"], tokenized_meta["offsets"]):
                 #subword-tokens have special chars depending on model type. In order to align with original text we need to get rid of them
-                special_chars = ["##", "Ġ", "▁"]
-                for s in special_chars:
-                    tok = tok.replace(s, "")
+                tok = re.sub(r"^(##|Ġ|▁)", "", tok)
+                #tok = tokenizer.decode(tokenizer.convert_tokens_to_ids(tok))
                 original_tok = text[offset:offset+len(tok)]
-               # assert tok == original_tok, f"Offset alignment wrong for {tokenizer.__class__.__name__} and text '{text}'"
+                assert tok == original_tok, f"Offset alignment wrong for {tokenizer.__class__.__name__} and text '{text}'"
 
 
 
@@ -122,7 +130,7 @@ def test_bert_custom_vocab(caplog):
         do_lower_case=False,
         never_split_chars=None)
 
-    tokenizer.add_custom_vocab("samples/tokenizer/custom_vocab.txt")
+    #deprecated: tokenizer.add_custom_vocab("samples/tokenizer/custom_vocab.txt")
     tokenizer.add_tokens(new_tokens=["neverseentokens"])
 
     basic_text = "Some Text with neverseentokens plus !215?#. and a combined-token_with/chars"
@@ -138,4 +146,4 @@ def test_bert_custom_vocab(caplog):
     assert tokenized_meta["start_of_word"] == [True, True, True, True, True, True, False, False, False, False, True, True, True, False, False, False, False, False, False, False]
 
 if(__name__=="__main__"):
-    test_bert_tokenizer()
+    test_all_tokenizer_on_special_cases()
