891f1c10788e259621a9908a426f82510fb1398c
Tanay Soni <tanaysoni12@gmail.com>
2019-10-25 16:34:30 +0200

    Add an option to disable multiprocessing in Inferencer(#117)
    Former-commit-id: 7f2203e7eca069804033d0c7e27a4efdc239681d

--- farm/inference_rest_api.py
+++ farm/inference_rest_api.py
@@ -79,7 +79,7 @@ class InferenceEndpoint(Resource):
         dicts = request.get_json().get("input", None)
         if not dicts:
             return {}
-        results = model.inference_from_dicts(dicts=dicts, rest_api_schema=True)
+        results = model.inference_from_dicts(dicts=dicts, rest_api_schema=True, use_multiprocessing=False)
         return results[0]
 
 
