41644de7a0b8331b6d1ba1aeca9c77431509b4dc
timoeller <timo.moeller@deepset.ai>
2019-11-13 15:43:54 +0100

    Bugfix tokenizer loading in examples and docs

--- examples/ner.py
+++ examples/ner.py
@@ -32,7 +32,7 @@ evaluate_every = 100
 lang_model = "bert-base-german-cased"
 
 # 1.Create a tokenizer
-tokenizer = Tokenizer.from_pretrained(
+tokenizer = Tokenizer.load(
     pretrained_model_name_or_path=lang_model, do_lower_case=False
 )
 
