65ea9b72a9243cd998d38fd1922612126098d2fd
Malte Pietsch <malte.pietsch@deepset.ai>
2020-01-16 13:48:34 +0100

    remove use_gpu arg and add arg deterministic_cudnn

--- farm/utils.py
+++ farm/utils.py
@@ -15,13 +15,25 @@ from farm.visual.ascii.images import WELCOME_BARN, WORKER_M, WORKER_F, WORKER_X
 logger = logging.getLogger(__name__)
 
 
-def set_all_seeds(seed, n_gpu=0):
+def set_all_seeds(seed, deterministic_cudnn=False):
+    """
+    Setting multiple seeds to make runs reproducible.
+
+    Important: Enabling `deterministic_cudnn` gives you full reproducibility with CUDA,
+    but might slow down your training (see https://pytorch.org/docs/stable/notes/randomness.html#cudnn) !
+
+    :param seed:number to use as seed
+    :type seed: int
+    :param deterministic_torch: Enable for full reproducibility when using CUDA. Caution: might slow down training.
+    :type deterministic_cudnn: bool
+    :return: None
+    """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
-    if n_gpu > 0:
-        torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed_all(seed)
+    if deterministic_cudnn:
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
