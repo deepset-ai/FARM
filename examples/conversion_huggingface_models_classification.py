3e55a1ff995b6896a4e6f09433bb80150d310ef3
Malte Pietsch <malte.pietsch@deepset.ai>
2020-01-22 08:11:02 +0100

    update examples for conversion
    Former-commit-id: e646a3a11d41dc706ac31f5b790221e1eb587fb8

--- examples/conversion_huggingface_models_classification.py
+++ examples/conversion_huggingface_models_classification.py
@@ -10,49 +10,54 @@ from pathlib import Path
 ##############################################
 ###  From Transformers -> FARM
 ##############################################
-# #
-transformers_input_name = "deepset/bert-base-german-cased-hatespeech-GermEval18Coarse"
-farm_output_dir = "../saved_models/farm-bert-base-german-cased-hatespeech-GermEval18Coarse"
+def convert_from_transformers():
+    transformers_input_name = "deepset/bert-base-german-cased-hatespeech-GermEval18Coarse"
+    farm_output_dir = Path("../saved_models/farm-bert-base-german-cased-hatespeech-GermEval18Coarse")
 
-# # CASE 1: MODEL
-# # Load model from transformers model hub (-> continue training / compare models / ...)
-model = AdaptiveModel.convert_from_transformers(transformers_input_name, device="cpu", task_type="text_classification")
-# # ... continue as in the other examples e.g. to fine-tune this QA model on your own data
-#
-# # CASE 2: INFERENCER
-# # Load Inferencer from transformers, incl. model & tokenizer (-> just get predictions)
-nlp = Inferencer.load(transformers_input_name, task_type="text_classification")
-#
-# # run predictions
-result = nlp.inference_from_dicts(dicts=[{"text": "Was ein scheiß Nazi!"}], rest_api_schema=True)
-pprint.pprint(result)
+    # # CASE 1: MODEL
+    # # Load model from transformers model hub (-> continue training / compare models / ...)
+    model = AdaptiveModel.convert_from_transformers(transformers_input_name, device="cpu", task_type="text_classification")
+    # # ... continue as in the other examples e.g. to fine-tune this QA model on your own data
+    #
+    # # CASE 2: INFERENCER
+    # # Load Inferencer from transformers, incl. model & tokenizer (-> just get predictions)
+    nlp = Inferencer.load(transformers_input_name, task_type="text_classification")
+    #
+    # # run predictions
+    result = nlp.inference_from_dicts(dicts=[{"text": "Was ein scheiß Nazi!"}], rest_api_schema=True)
+    pprint.pprint(result)
 
-# save it
-nlp.save(farm_output_dir)
+    # save it
+    nlp.save(farm_output_dir)
 
 # ##############################################
 # ###  From FARM -> Transformers
 # ##############################################
-farm_input_dir = "../saved_models/farm-bert-base-german-cased-german-GermEval18Coarse"
-transformers_output_dir = "../saved_models/bert-base-german-cased-hatespeech-GermEval18Coarse"
-#
-# # # load from FARM format
-model = AdaptiveModel.load(farm_input_dir, device="cpu")
-processor = Processor.load_from_dir(farm_input_dir)
-model.connect_heads_with_processor(processor.tasks)
+def convert_to_transformers():
+    farm_input_dir = Path("../saved_models/farm-bert-base-german-cased-german-GermEval18Coarse")
+    transformers_output_dir = "../saved_models/bert-base-german-cased-hatespeech-GermEval18Coarse"
+    #
+    # # # load from FARM format
+    model = AdaptiveModel.load(farm_input_dir, device="cpu")
+    processor = Processor.load_from_dir(farm_input_dir)
+    model.connect_heads_with_processor(processor.tasks)
 
-# convert to transformers
-transformer_model = model.convert_to_transformers()
+    # convert to transformers
+    transformer_model = model.convert_to_transformers()
 
-# save it
-Path(transformers_output_dir).mkdir(parents=True, exist_ok=True)
-transformer_model.save_pretrained(transformers_output_dir)
-processor.tokenizer.save_pretrained(transformers_output_dir)
+    # save it (note: transformers use str instead of Path objects)
+    Path(transformers_output_dir).mkdir(parents=True, exist_ok=True)
+    transformer_model.save_pretrained(transformers_output_dir)
+    processor.tokenizer.save_pretrained(transformers_output_dir)
 
-# run predictions (using transformers)
-nlp = pipeline('sentiment-analysis', model=str(transformers_output_dir), tokenizer=str(transformers_output_dir))
-res = nlp("Was ein scheiß Nazi!")
-pprint.pprint(res)
+    # run predictions (using transformers)
+    nlp = pipeline('sentiment-analysis', model=str(transformers_output_dir), tokenizer=str(transformers_output_dir))
+    res = nlp("Was ein scheiß Nazi!")
+    pprint.pprint(res)
 
-# # To upload to transformer's model hub run this in bash:
-# # transformers-cli upload  ../saved_models/bert-large-uncased-whole-word-masking-squad2
+    # # To upload to transformer's model hub run this in bash:
+    # # transformers-cli upload  ../saved_models/bert-large-uncased-whole-word-masking-squad2
+
+if __name__ == "__main__":
+    convert_from_transformers()
+    convert_to_transformers()
