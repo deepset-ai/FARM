b6445d20e576e0f0aed3c29991bde0a7ddade777
Malte Pietsch <malte.pietsch@deepset.ai>
2020-01-31 16:42:05 +0100

    Add option for custom loss aggregation of heads (#220)
    Former-commit-id: 30b4bf2119da05ab346d42cda31450f53b954294

--- farm/modeling/adaptive_model.py
+++ farm/modeling/adaptive_model.py
@@ -22,7 +22,8 @@ class AdaptiveModel(nn.Module):
         prediction_heads,
         embeds_dropout_prob,
         lm_output_types,
-        device
+        device,
+        loss_aggregation_fn=None
     ):
         """
         :param language_model: Any model that turns token ids into vector representations
@@ -39,7 +40,19 @@ class AdaptiveModel(nn.Module):
                                 one for each prediction head.
         :type lm_output_types: list or str
         :param device: The device on which this model will operate. Either "cpu" or "cuda".
+        :param loss_aggregation_fn: Function to aggregate the loss of multiple prediction heads.
+                                    Input: loss_per_head (list of tensors), global_step (int), batch (dict)
+                                    Output: aggregated loss (tensor)
+                                    Default is a simple sum:
+                                            lambda loss_per_head, global_step=None, batch=None: sum(tensors)
+                                    However, you can pass more complex functions that depend on the
+                                    current step (e.g. for round-robin style multitask learning) or the actual
+                                    content of the batch (e.g. certain labels)
+                                    Note: The loss at this stage is per sample, i.e one tensor of
+                                    shape (batchsize) per prediction head.
+        :type loss_aggregation_fn: function
         """
+
         super(AdaptiveModel, self).__init__()
         self.device = device
         self.language_model = language_model.to(device)
@@ -55,6 +68,10 @@ class AdaptiveModel(nn.Module):
             [lm_output_types] if isinstance(lm_output_types, str) else lm_output_types
         )
         self.log_params()
+        # default loss aggregation function is a simple sum (without using any of the optional params)
+        if not loss_aggregation_fn:
+            loss_aggregation_fn = lambda loss_per_head, global_step=None, batch=None: sum(loss_per_head)
+        self.loss_aggregation_fn = loss_aggregation_fn
 
     def fit_heads_to_lm(self):
         """This iterates over each prediction head and ensures that its input dimensionality matches the output
@@ -135,21 +152,23 @@ class AdaptiveModel(nn.Module):
             all_losses.append(head.logits_to_loss(logits=logits_for_one_head, **kwargs))
         return all_losses
 
-    def logits_to_loss(self, logits, **kwargs):
+    def logits_to_loss(self, logits, global_step=None, **kwargs):
         """
         Get losses from all prediction heads & reduce to single loss *per sample*.
 
         :param logits: logits, can vary in shape and type, depending on task
         :type logits: object
-        :param kwargs: placeholder for passing generic parameters
+        :param global_step: number of current training step
+        :type global_step: int
+        :param kwargs: placeholder for passing generic parameters.
+                       Note: Contains the batch (as dict of tensors), when called from Trainer.train().
         :type kwargs: object
         :return loss: torch.tensor that is the per sample loss (len: batch_size)
         """
         all_losses = self.logits_to_loss_per_head(logits, **kwargs)
-        # this sums up loss per sample across multiple prediction heads
-        # TODO, check if we should take mean here.
-        # Otherwise we have to scale the learning rate in relation to how many Prediction Heads we have
-        loss = sum(all_losses)
+        # This aggregates the loss per sample across multiple prediction heads
+        # Default is sum(), but you can configure any fn that takes [Tensor, Tensor ...] and returns [Tensor]
+        loss = self.loss_aggregation_fn(all_losses, global_step=global_step, batch=kwargs)
         return loss
 
     def logits_to_preds(self, logits, **kwargs):
