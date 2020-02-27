1dc1b202f10feae2fb99c84b8b9f53bcf05bb8be
Tanay Soni <tanaysoni12@gmail.com>
2020-02-27 18:41:50 +0100

    Update test fixture to download from model hub

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,3 @@
-import os
-from pathlib import Path
-
-import boto3
 import pytest
 
 from farm.infer import Inferencer
@@ -9,13 +5,5 @@ from farm.infer import Inferencer
 
 @pytest.fixture(scope="module")
 def adaptive_model_qa():
-    # download the model from S3
-    s3_resource = boto3.resource("s3")
-    bucket = s3_resource.Bucket("deepset.ai-farm-models")
-    prefix = "0.3.0/bert-english-qa-large/"
-    for object in bucket.objects.filter(Prefix=prefix):
-        if not os.path.exists(Path(object.key).parent):
-            os.makedirs(os.path.dirname(object.key))
-        bucket.download_file(object.key, object.key)
-    model = Inferencer.load(Path(prefix), batch_size=16)
+    model = Inferencer.load("deepset/bert-base-cased-squad2", task_type="question_answering", batch_size=16)
     return model
