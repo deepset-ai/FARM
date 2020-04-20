a8225dd4a2b5d2afd7c5b976938c47578798e4ef
Branden Chan <brandenchan@icloud.com>
2020-04-20 11:06:18 +0200

    Fix another test error

--- test/test_ner_amp.py
+++ test/test_ner_amp.py
@@ -92,7 +92,7 @@ def test_ner_amp(caplog):
     result = model.inference_from_dicts(dicts=basic_texts)
 
     assert result[0]["predictions"][0][0]["context"] == "Crown"
-    assert isinstance(result[0]["predictions"][0]["probability"], np.float32)
+    assert isinstance(result[0]["predictions"][0][0]["probability"], np.float32)
 
 
 if __name__ == "__main__":
