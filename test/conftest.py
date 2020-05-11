e2d653f956dce345c6784be1c5ae8ba3d9e4a76f
Tanay Soni <tanaysoni12@gmail.com>
2020-05-11 18:31:51 +0200

    Add inference speed benchmarking pipeline (#321)
    Former-commit-id: 397bbaa880e315c4918ea114da35f76ea9063607

--- test/conftest.py
+++ test/conftest.py
@@ -3,9 +3,35 @@ import pytest
 from farm.infer import Inferencer
 
 
+def pytest_addoption(parser):
+    """
+    Hook to pass pytest-fixture arguments to tests from the command line.
+    """
+    parser.addoption("--use_gpu", action="store_true", default=False)
+
+
+def pytest_generate_tests(metafunc):
+    """
+    This method gets called for all test cases. Here, we set the arguments supplied in pytest_addoption().
+    """
+    option_value = metafunc.config.option.use_gpu
+    if 'use_gpu' in metafunc.fixturenames:
+        if option_value:
+            metafunc.parametrize("use_gpu", [True], scope="session")
+        else:
+            metafunc.parametrize("use_gpu", [False], scope="session")
+
+
 @pytest.fixture(scope="session")
-def adaptive_model_qa(num_processes=0):
+def adaptive_model_qa(use_gpu, num_processes):
+    """
+    PyTest Fixture for a Question Answering Inferencer based on PyTorch.
+    """
     model = Inferencer.load(
-        "deepset/bert-base-cased-squad2", task_type="question_answering", batch_size=16, num_processes=num_processes
+        "deepset/bert-base-cased-squad2",
+        task_type="question_answering",
+        batch_size=16,
+        num_processes=num_processes,
+        gpu=use_gpu,
     )
     return model
