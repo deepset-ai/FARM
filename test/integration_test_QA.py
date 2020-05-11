3fc0ff8929a950fa796f3786efb15594f8f0714a
Timo Moeller <timo.moeller@deepset.ai>
2020-05-11 17:43:08 +0200

    Add qa accuracy benchmarks

--- test/integration_test_QA.py
+++ test/integration_test_QA.py
@@ -1,31 +1,39 @@
-from farm.infer import Inferencer
-from farm.data_handler.utils import write_squad_predictions
-from farm.utils import initialize_device_settings
-from farm.modeling.tokenization import Tokenizer
-from farm.data_handler.processor import SquadProcessor
-from farm.data_handler.data_silo import DataSilo
-from farm.eval import Evaluator
-from farm.evaluation import squad_evaluation
-from farm.modeling.adaptive_model import AdaptiveModel
-from pathlib import Path
-import numpy as np
-from dotmap import DotMap
-from time import time
 import logging
 import os
+from pathlib import Path
+from time import time
+
+import numpy as np
+from dotmap import DotMap
+
+from farm.data_handler.data_silo import DataSilo
+from farm.data_handler.processor import SquadProcessor
+from farm.data_handler.utils import write_squad_predictions
+from farm.eval import Evaluator
+from farm.evaluation import squad_evaluation
+from farm.infer import Inferencer
+from farm.modeling.adaptive_model import AdaptiveModel
+from farm.modeling.language_model import LanguageModel
+from farm.modeling.optimization import initialize_optimizer
+from farm.modeling.prediction_head import QuestionAnsweringHead
+from farm.modeling.tokenization import Tokenizer
+from farm.train import Trainer
+from farm.utils import set_all_seeds, initialize_device_settings
+
 
 def test_evaluation():
     ##########################
     ########## Settings
     ##########################
     lang_model = "deepset/roberta-base-squad2"
-    do_lower_case = True
+    do_lower_case = False
+
+    test_assertions = True
 
     data_dir = Path("testsave/data/squad20")
     evaluation_filename = "dev-v2.0.json"
 
     device, n_gpu = initialize_device_settings(use_cuda=True)
-
 
     # loading models and evals
     model = AdaptiveModel.convert_from_transformers(lang_model, device=device, task_type="question_answering")
@@ -58,21 +66,23 @@ def test_evaluation():
     em_score = results[0]["EM"]*100
     tnrecall = results[0]["top_n_recall"]*100
     elapsed = time() - starttime
+    print(results)
     print(elapsed)
 
-    gold_f1 = 80.56469
-    gold_EM = 75.73486
-    gold_tnrecall = 82.39703
-    gold_elapsed = 95.11 ## TBD
-    np.testing.assert_allclose(f1_score, gold_f1, rtol=0.001, err_msg=f"FARM Eval changed for f1 score by: {f1_score-gold_f1}")
-    np.testing.assert_allclose(em_score, gold_EM, rtol=0.001, err_msg=f"FARM Eval changed for EM by: {em_score-gold_EM}")
-    np.testing.assert_allclose(tnrecall, gold_tnrecall, rtol=0.001, err_msg=f"FARM Eval changed for top 1 recall by: {em_score-gold_EM}")
-    #np.testing.assert_allclose(elapsed, gold_elapsed, rtol=0.1, err_msg=f"FARM Eval speed changed significantly: {elapsed - gold_elapsed}")
+    gold_EM = 77.7478
+    gold_f1 = 82.1557
+    gold_tnrecall = 84.0646 # top 1 recall
+    gold_elapsed = 78 # 4x V100
+    if test_assertions:
+        np.testing.assert_allclose(em_score, gold_EM, rtol=0.001, err_msg=f"FARM Eval changed for EM by: {em_score-gold_EM}")
+        np.testing.assert_allclose(f1_score, gold_f1, rtol=0.001, err_msg=f"FARM Eval changed for f1 score by: {f1_score-gold_f1}")
+        np.testing.assert_allclose(tnrecall, gold_tnrecall, rtol=0.001, err_msg=f"FARM Eval changed for top 1 recall by: {em_score-gold_EM}")
+        np.testing.assert_allclose(elapsed, gold_elapsed, rtol=0.1, err_msg=f"FARM Eval speed changed significantly: {elapsed - gold_elapsed}")
 
 
     # 2. Test FARM predictions with outside eval script
     starttime = time()
-    model = Inferencer(model=model, processor=processor, task_type="question_answering",batch_size=50, gpu=device.type=="cuda")
+    model = Inferencer(model=model, processor=processor, task_type="question_answering", batch_size=50, gpu=device.type=="cuda")
     filename = data_dir / evaluation_filename
     result = model.inference_from_file(file=filename)
 
@@ -93,17 +103,111 @@ def test_evaluation():
     f1_score = results_official["f1"]
     em_score = results_official["exact"]
 
-    gold_f1 = 80.03626
-    gold_EM = 76.69502
-    gold_elapsed = 76.035 ## TBD
-    np.testing.assert_allclose(f1_score, gold_f1, rtol=0.001,
-                               err_msg=f"Eval with official script changed for f1 score by: {f1_score - gold_f1}")
-    np.testing.assert_allclose(em_score, gold_EM, rtol=0.001,
-                               err_msg=f"Eval with official script changed for EM by: {em_score - gold_EM}")
-    np.testing.assert_allclose(elapsed, gold_elapsed, rtol=0.1,
-                               err_msg=f"Inference speed changed significantly: {elapsed - gold_elapsed}")
+    gold_EM = 78.489
+    gold_f1 = 81.7104
+    gold_elapsed = 74 # 4x V100
+    print(elapsed)
+    if test_assertions:
+        np.testing.assert_allclose(em_score, gold_EM, rtol=0.001,
+                                   err_msg=f"Eval with official script changed for EM by: {em_score - gold_EM}")
+        np.testing.assert_allclose(f1_score, gold_f1, rtol=0.001,
+                                   err_msg=f"Eval with official script changed for f1 score by: {f1_score - gold_f1}")
+        np.testing.assert_allclose(elapsed, gold_elapsed, rtol=0.1,
+                                   err_msg=f"Inference speed changed significantly: {elapsed - gold_elapsed}")
 
+
+def train_evaluation_single(seed=42):
+    ##########################
+    ########## Settings
+    ##########################
+    set_all_seeds(seed=seed)
+    device, n_gpu = initialize_device_settings(use_cuda=True)
+    batch_size = 32*4 # 4x V100
+    n_epochs = 2
+    evaluate_every = 2000000 # disabling dev eval
+    lang_model = "roberta-base"
+    do_lower_case = False  # roberta is a cased model
+    train_filename = "train-v2.0.json"
+    dev_filename = "dev-v2.0.json"
+
+
+    # Load model and train
+    tokenizer = Tokenizer.load(
+        pretrained_model_name_or_path=lang_model, do_lower_case=do_lower_case
+    )
+    processor = SquadProcessor(
+        tokenizer=tokenizer,
+        max_seq_len=256,
+        label_list=["start_token", "end_token"],
+        metric="squad",
+        train_filename=train_filename,
+        dev_filename=dev_filename,
+        test_filename=None,
+        data_dir=Path("testsave/data/squad20"),
+    )
+    data_silo = DataSilo(processor=processor, batch_size=batch_size, distributed=False)
+    language_model = LanguageModel.load(lang_model)
+    prediction_head = QuestionAnsweringHead()
+    model = AdaptiveModel(
+        language_model=language_model,
+        prediction_heads=[prediction_head],
+        embeds_dropout_prob=0.1,
+        lm_output_types=["per_token"],
+        device=device,
+    )
+    model, optimizer, lr_schedule = initialize_optimizer(
+        model=model,
+        learning_rate=3e-5,
+        schedule_opts={"name": "LinearWarmup", "warmup_proportion": 0.2},
+        n_batches=len(data_silo.loaders["train"]),
+        n_epochs=n_epochs,
+        device=device
+    )
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+        data_silo=data_silo,
+        epochs=n_epochs,
+        n_gpu=n_gpu,
+        lr_schedule=lr_schedule,
+        evaluate_every=evaluate_every,
+        device=device,
+    )
+    starttime = time()
+    trainer.train()
+    elapsed = time() - starttime
+
+    save_dir = Path("testsave/roberta-qa-dev")
+    model.save(save_dir)
+    processor.save(save_dir)
+
+    # Create Evaluator
+    evaluator = Evaluator(data_loader=data_silo.get_data_loader("dev"), tasks=data_silo.processor.tasks, device=device)
+
+    results = evaluator.eval(model)
+    f1_score = results[0]["f1"] * 100
+    em_score = results[0]["EM"] * 100
+    tnrecall = results[0]["top_n_recall"] * 100
+
+    print(results)
+    print(elapsed)
+
+
+    gold_f1 = 82.155
+    gold_EM = 77.714
+    gold_tnrecall = 97.3721 #
+    gold_elapsed = 1286.30
+    np.testing.assert_allclose(f1_score, gold_f1, rtol=0.01,
+                               err_msg=f"FARM Training changed for f1 score by: {f1_score - gold_f1}")
+    np.testing.assert_allclose(em_score, gold_EM, rtol=0.01,
+                               err_msg=f"FARM Training changed for EM by: {em_score - gold_EM}")
+    np.testing.assert_allclose(tnrecall, gold_tnrecall, rtol=0.01,
+                               err_msg=f"FARM Training changed for top 1 recall by: {em_score - gold_EM}")
+    np.testing.assert_allclose(elapsed, gold_elapsed, rtol=0.1, err_msg=f"FARM Eval speed changed significantly: {elapsed - gold_elapsed}")
 
 if __name__ == "__main__":
     logging.disable(logging.WARNING)
+
     test_evaluation()
+
+    train_evaluation_single(seed=42)
