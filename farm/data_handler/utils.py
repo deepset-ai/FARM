1c5be412728111631718a87c61c1883509eb43a1
Tanay Soni <tanaysoni12@gmail.com>
2020-05-18 16:23:40 +0200

    Write remainder docs to file in randomize_and_split_file()

--- farm/data_handler/utils.py
+++ farm/data_handler/utils.py
@@ -770,3 +770,13 @@ def randomize_and_split_file(filepath, output_dir, docs_per_file=1_000, delimite
                     write_file.close()
                     output_file_number += 1
                     lines_to_write = []
+
+        if lines_to_write:
+            filename = output_dir / f"part_{output_file_number}"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            write_file = stack.enter_context(open(filename, 'w+', buffering=10 * 1024 * 1024))
+            random.shuffle(lines_to_write)
+            write_file.writelines(lines_to_write)
+            write_file.close()
+
+    logger.info(f"The input file {filepath} is split in {output_file_number} parts at {output_dir}.")
