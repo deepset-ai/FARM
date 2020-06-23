dc74b6ba7d8f4e3b4aea56097439048124e5e3f1
Timo Moeller <timo.moeller@deepset.ai>
2020-06-23 16:12:26 +0200

    Add model optimization to inference loading (#415)
    * Add model optimization to inference loading
    
    * Add check for onnx model
    
    * make optimize model function public
    
    Former-commit-id: 56b6095f14bcecfe214bdbbc066732ad942fd0c3

--- farm/infer.py
+++ farm/infer.py
@@ -12,7 +12,8 @@ from farm.data_handler.dataloader import NamedDataLoader
 from farm.data_handler.processor import Processor, InferenceProcessor, SquadProcessor, NERProcessor, TextClassificationProcessor
 from farm.data_handler.utils import grouper
 from farm.modeling.tokenization import Tokenizer
-from farm.modeling.adaptive_model import AdaptiveModel, BaseAdaptiveModel
+from farm.modeling.adaptive_model import AdaptiveModel, BaseAdaptiveModel, ONNXAdaptiveModel
+from farm.modeling.optimization import optimize_model
 from farm.utils import initialize_device_settings
 from farm.utils import set_all_seeds, calc_chunksize, log_ascii_workers
 
@@ -244,6 +245,8 @@ class Inferencer:
                                  f"Valid options for arg `task_type`: 'question_answering', "
                                  f"'embeddings', 'text_classification', 'ner'")
 
+        if not isinstance(model,ONNXAdaptiveModel):
+            model, _ = optimize_model(model=model, device=device, local_rank=-1, optimizer=None)
         return cls(
             model,
             processor,
