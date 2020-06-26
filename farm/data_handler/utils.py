cc8741e0a4607c847c4671ed8bbfbf11ea989a71
Tanay Soni <tanaysoni12@gmail.com>
2020-06-26 11:30:50 +0200

    Remove randomization in individual files when splitting a train file

--- farm/data_handler/utils.py
+++ farm/data_handler/utils.py
@@ -767,7 +767,7 @@ def grouper(iterable, n, worker_id=0, total_workers=1):
     return iter(lambda: list(islice(iterable, n)), [])
 
 
-def randomize_and_split_file(filepath, output_dir, docs_per_file=1_000, delimiter="", encoding="utf-8"):
+def split_file(filepath, output_dir, docs_per_file=1_000, delimiter="", encoding="utf-8"):
     total_lines = sum(1 for line in open(filepath, encoding=encoding))
     output_file_number = 1
     doc_count = 0
@@ -782,7 +782,6 @@ def randomize_and_split_file(filepath, output_dir, docs_per_file=1_000, delimite
                     filename = output_dir / f"part_{output_file_number}"
                     os.makedirs(os.path.dirname(filename), exist_ok=True)
                     write_file = stack.enter_context(open(filename, 'w+', buffering=10 * 1024 * 1024))
-                    random.shuffle(lines_to_write)
                     write_file.writelines(lines_to_write)
                     write_file.close()
                     output_file_number += 1
@@ -792,7 +791,6 @@ def randomize_and_split_file(filepath, output_dir, docs_per_file=1_000, delimite
             filename = output_dir / f"part_{output_file_number}"
             os.makedirs(os.path.dirname(filename), exist_ok=True)
             write_file = stack.enter_context(open(filename, 'w+', buffering=10 * 1024 * 1024))
-            random.shuffle(lines_to_write)
             write_file.writelines(lines_to_write)
             write_file.close()
 
