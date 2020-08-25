Make use of fast tokenizer possible + fix bug in offset calculation

test/test_inference.py
@@ -98,7 +98,7 @@ def test_embeddings_extraction(num_processes, use_fast):
     result = model.inference_from_dicts(dicts=basic_texts)
     assert result[0]["context"] == ['Schar', '##tau', 'sagte', 'dem', 'Tages', '##spiegel', ',', 'dass', 'Fischer', 'ein', 'Id', '##iot', 'ist']
     assert result[0]["vec"].shape == (768,)
-    assert np.isclose(result[0]["vec"][0], -0.032460204579613426)
+    assert np.isclose(result[0]["vec"][0], 0.01501756374325071)
 
 
 def test_inferencer_with_fast_bert_tokenizer():