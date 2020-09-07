d7b5cb61f062436789aa9081a85ade4f56e2db6f
Timo Moeller <timo.moeller@deepset.ai>
2020-09-07 12:47:17 +0200

    fix Natural Question inference processing (#521)
    Add try catch around ID extraction

--- farm/utils.py
+++ farm/utils.py
@@ -427,12 +427,15 @@ def stack(list_of_lists):
 
 
 def try_get(keys, dictionary):
-    for key in keys:
-        if key in dictionary:
-            ret = dictionary[key]
-            if type(ret) == list:
-                ret = ret[0]
-            return ret
+    try:
+        for key in keys:
+            if key in dictionary:
+                ret = dictionary[key]
+                if type(ret) == list:
+                    ret = ret[0]
+                return ret
+    except Exception as e:
+        logger.warning(f"Cannot extract from dict {dictionary} with error: {e}")
     return None
 
 class Benchmarker:
