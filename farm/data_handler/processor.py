f1f132e2eda73055e70ea4d06d40dac19191b9c9
kolk <tainted.sword@gmail.com>
2020-10-08 16:25:28 +0530

    Saving models fixed, PredictionHead and Processor names refactored
    Former-commit-id: 0f36335598b7350577ff55bc67240cf56f04c016
    Former-commit-id: dd3d33ce0d0099ef65e685736e8d8a1427b7ef2c

--- farm/data_handler/processor.py
+++ farm/data_handler/processor.py
@@ -1728,7 +1728,7 @@ class RegressionProcessor(Processor):
         )
         return features
 
-class DPRProcessor(Processor):
+class TextSimilarityProcessor(Processor):
     """
     Used to handle the text DPR datasets that come in json format
     """
@@ -1794,7 +1794,7 @@ class DPRProcessor(Processor):
         self.num_hard_negatives = num_hard_negatives
         self.num_positives = num_positives
 
-        super(DPRProcessor, self).__init__(
+        super(TextSimilarityProcessor, self).__init__(
             tokenizer=tokenizer,
             max_seq_len=max_seq_len,
             train_filename=train_filename,
@@ -1806,12 +1806,11 @@ class DPRProcessor(Processor):
             proxies=proxies,
         )
         if metric:
-            task_type = "representation_learning"
-            self.add_task(name="representation_learning",
+            self.add_task(name="text_similarity",
                           metric=metric,
                           label_list=label_list,
                           label_name="label",
-                          task_type=task_type)
+                          task_type="text_similarity")
         else:
             logger.info("Initialized processor without tasks. Supply `metric` and `label_list` to the constructor for "
                         "using the default task or add a custom task later via processor.add_task()")
@@ -1819,10 +1818,10 @@ class DPRProcessor(Processor):
     @classmethod
     def load_from_dir(cls, load_dir):
         """
-         Overwriting method from parent class to **always** load the DPRProcessor instead of the specific class stored in the config.
+         Overwriting method from parent class to **always** load the TextSimilarityProcessor instead of the specific class stored in the config.
 
         :param load_dir: str, directory that contains a 'processor_config.json'
-        :return: An instance of an DPRProcessor
+        :return: An instance of an TextSimilarityProcessor
         """
         # read config
         processor_config_file = Path(load_dir) / "processor_config.json"
@@ -1835,7 +1834,7 @@ class DPRProcessor(Processor):
         del config["tokenizer"]
         del config["passage_tokenizer"]
 
-        processor = cls.load(tokenizer=tokenizer, passage_tokenizer=passage_tokenizer, processor_name="DPRProcessor", **config)
+        processor = cls.load(tokenizer=tokenizer, passage_tokenizer=passage_tokenizer, processor_name="TextSimilarityProcessor", **config)
         for task_name, task in config["tasks"].items():
             processor.add_task(name=task_name, metric=task["metric"], label_list=task["label_list"])
 
