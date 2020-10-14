c8112a06699ec3cf4f04af15b36a2ce3bb37a42f
kolk <tainted.sword@gmail.com>
2020-10-14 12:23:24 +0530

    bug fix in intitalizing DPR language model
    Former-commit-id: 9b083c3c7fc64dfba5e6409b4df23fb1bccd4124
    Former-commit-id: e81d4f8888e1bad1623a794a80a7558d3a38e333

--- examples/dpr_encoder.py
+++ examples/dpr_encoder.py
@@ -5,15 +5,15 @@ import pprint
 from pathlib import Path
 
 from farm.data_handler.data_silo import DataSilo
-from farm.data_handler.processor import DPRProcessor
+from farm.data_handler.processor import TextSimilarityProcessor
 from farm.modeling.biadaptive_model import BiAdaptiveModel
 from farm.modeling.language_model import LanguageModel
 from farm.modeling.optimization import initialize_optimizer
-from farm.modeling.prediction_head import DensePassageRetrievalHead
+from farm.modeling.prediction_head import TextSimilarityHead
 from farm.modeling.tokenization import Tokenizer
 from farm.train import Trainer
 from farm.utils import set_all_seeds, MLFlowLogger, initialize_device_settings
-import torch
+from farm.eval import Evaluator
 
 def dense_passage_retrieval():
     logging.basicConfig(
@@ -30,9 +30,9 @@ def dense_passage_retrieval():
     ##########################
     set_all_seeds(seed=42)
     device, n_gpu = initialize_device_settings(use_cuda=True)
-    batch_size = 2
+    batch_size = 14
     n_epochs = 3
-    evaluate_every = 3258
+    evaluate_every = 1000
     question_lang_model = "facebook/dpr-question_encoder-single-nq-base"
     passage_lang_model = "facebook/dpr-ctx_encoder-single-nq-base"
     do_lower_case = True
@@ -51,32 +51,32 @@ def dense_passage_retrieval():
 
     # 2. Create a DataProcessor that handles all the conversion from raw text into a pytorch Dataset
     label_list = ["hard_negative", "positive"]
-    metric = "representation_learning"
-    processor = DPRProcessor(tokenizer=query_tokenizer,
+    metric = "text_similarity_metric"
+    processor = TextSimilarityProcessor(tokenizer=query_tokenizer,
                              passage_tokenizer=context_tokenizer,
                              max_seq_len=512,
                              label_list=label_list,
                              metric=metric,
                              data_dir="data/retriever",
-                             train_filename="nq-train.json",
-                             dev_filename="nq-dev.json",
-                             test_filename="nq-dev.json",
+                             train_filename=train_filename,
+                             dev_filename=dev_filename,
+                             test_filename=dev_filename,
                              embed_title=embed_title,
                              num_hard_negatives=num_hard_negatives)
 
     # 3. Create a DataSilo that loads several datasets (train/dev/test), provides DataLoaders for them and calculates a few descriptive statistics of our datasets
     # NOTE: In FARM, the dev set metrics differ from test set metrics in that they are calculated on a token level instead of a word level
-    data_silo = DataSilo(processor=processor, batch_size=batch_size, distributed=False)#, max_processes=1)
+    data_silo = DataSilo(processor=processor, batch_size=batch_size, distributed=False)
 
 
     # 4. Create an AdaptiveModel+
     # a) which consists of a pretrained language model as a basis
-    question_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased", language_model_class="DPRQuestion")
-    passage_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased", language_model_class="DPRContext")
+    question_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased", language_model_class="DPRQuestionEncoder")
+    passage_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased", language_model_class="DPRContextEncoder")
 
 
     # b) and a prediction head on top that is suited for our task => Question Answering
-    prediction_head = DensePassageRetrievalHead(similarity_function=similarity_function)
+    prediction_head = TextSimilarityHead(similarity_function=similarity_function)
 
     model = BiAdaptiveModel(
         language_model1=question_language_model,
@@ -88,9 +88,6 @@ def dense_passage_retrieval():
         device=device,
     )
 
-    lm1_output_types = "per_sequence",
-    lm2_output_types = "per_sequence",
-    loss_aggregation_fn = None,
     # 5. Create an optimizer
     model, optimizer, lr_schedule = initialize_optimizer(
         model=model,
@@ -103,7 +100,6 @@ def dense_passage_retrieval():
         grad_acc_steps=1,
         device=device
     )
-
 
     # 6. Feed everything to the Trainer, which keeps care of growing our model and evaluates it from time to time
     trainer = Trainer(
@@ -119,6 +115,12 @@ def dense_passage_retrieval():
 
     # 7. Let it grow! Watch the tracked metrics live on the public mlflow server: https://public-mlflow.deepset.ai
     trainer.train()
+
+    test_data_loader = data_silo.get_data_loader("test")
+    if test_data_loader is not None:
+        evaluator_test = Evaluator(
+            data_loader=test_data_loader, tasks=data_silo.processor.tasks, device=device)
+        test_result = evaluator_test.eval(model)
 
     # 8. Hooray! You have a model. Store it:
     save_dir = Path("../saved_models/dpr-tutorial")
