966e7e19824a1ffe3be332c9dec7ac0fe6cc0d27
Branden Chan <brandenchan@icloud.com>
2020-10-22 17:55:22 +0200

    First attempt vectorize qa ph

--- farm/data_handler/processor.py
+++ farm/data_handler/processor.py
@@ -1268,6 +1268,7 @@ class SquadProcessor(QAProcessor):
                                          max_seq_len=self.max_seq_len,
                                          sp_toks_start=self.sp_toks_start,
                                          sp_toks_mid=self.sp_toks_mid,
+                                         sp_toks_end=self.sp_toks_end,
                                          max_answers=self.max_answers)
         return features
 
