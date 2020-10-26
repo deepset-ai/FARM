755f104cf8210743de5e48165d80e6b622ff59ce
Branden Chan <brandenchan@icloud.com>
2020-10-26 15:56:36 +0100

    fix nq processor

--- farm/data_handler/processor.py
+++ farm/data_handler/processor.py
@@ -1572,6 +1572,7 @@ class NaturalQuestionsProcessor(QAProcessor):
                                          max_seq_len=self.max_seq_len,
                                          sp_toks_start=self.sp_toks_start,
                                          sp_toks_mid=self.sp_toks_mid,
+                                         sp_toks_end=self.sp_toks_end,
                                          answer_type_list=self.answer_type_list,
                                          max_answers=self.max_answers)
         return features
