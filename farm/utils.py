6c82737fac4258758a84a3ca7cf7bc9352ee6979
Branden Chan <33759007+brandenchan@users.noreply.github.com>
2020-12-04 14:19:22 +0100

    MLFlowLogger can be disabled (#650)
    * Implement disable
    
    * Change code style
    
    * Change class attribute name
    
    Former-commit-id: 1ae1b718312efb07f45c6b454326df01af462f93
    Former-commit-id: feb85823be178c9fb104b3517f21dbeb64159808

--- farm/utils.py
+++ farm/utils.py
@@ -95,6 +95,8 @@ class BaseMLLogger:
     This class can be extended to implement custom logging backends like MLFlow, Tensorboard, or Sacred.
     """
 
+    disable_logging = False
+
     def __init__(self, tracking_uri, **kwargs):
         self.tracking_uri = tracking_uri
         print(WELCOME_BARN)
@@ -145,47 +147,57 @@ class MLFlowLogger(BaseMLLogger):
     """
 
     def init_experiment(self, experiment_name, run_name=None, nested=True):
-        try:
-            mlflow.set_tracking_uri(self.tracking_uri)
-            mlflow.set_experiment(experiment_name)
-            mlflow.start_run(run_name=run_name, nested=nested)
-        except ConnectionError:
-            raise Exception(
-                f"MLFlow cannot connect to the remote server at {self.tracking_uri}.\n"
-                f"MLFlow also supports logging runs locally to files. Set the MLFlowLogger "
-                f"tracking_uri to an empty string to use that."
-            )
+        if not self.disable_logging:
+            try:
+                mlflow.set_tracking_uri(self.tracking_uri)
+                mlflow.set_experiment(experiment_name)
+                mlflow.start_run(run_name=run_name, nested=nested)
+            except ConnectionError:
+                raise Exception(
+                    f"MLFlow cannot connect to the remote server at {self.tracking_uri}.\n"
+                    f"MLFlow also supports logging runs locally to files. Set the MLFlowLogger "
+                    f"tracking_uri to an empty string to use that."
+                )
 
     @classmethod
     def log_metrics(cls, metrics, step):
-        try:
-            mlflow.log_metrics(metrics, step=step)
-        except ConnectionError:
-            logger.warning(f"ConnectionError in logging metrics to MLFlow.")
-        except Exception as e:
-            logger.warning(f"Failed to log metrics: {e}")
+        if not cls.disable_logging:
+            try:
+                mlflow.log_metrics(metrics, step=step)
+            except ConnectionError:
+                logger.warning(f"ConnectionError in logging metrics to MLFlow.")
+            except Exception as e:
+                logger.warning(f"Failed to log metrics: {e}")
 
     @classmethod
     def log_params(cls, params):
-        try:
-            mlflow.log_params(params)
-        except ConnectionError:
-            logger.warning("ConnectionError in logging params to MLFlow")
-        except Exception as e:
-            logger.warning(f"Failed to log params: {e}")
+        if not cls.disable_logging:
+            try:
+                mlflow.log_params(params)
+            except ConnectionError:
+                logger.warning("ConnectionError in logging params to MLFlow")
+            except Exception as e:
+                logger.warning(f"Failed to log params: {e}")
 
     @classmethod
     def log_artifacts(cls, dir_path, artifact_path=None):
-        try:
-            mlflow.log_artifacts(dir_path, artifact_path)
-        except ConnectionError:
-            logger.warning(f"ConnectionError in logging artifacts to MLFlow")
-        except Exception as e:
-            logger.warning(f"Failed to log artifacts: {e}")
+        if not cls.disable_logging:
+            try:
+                mlflow.log_artifacts(dir_path, artifact_path)
+            except ConnectionError:
+                logger.warning(f"ConnectionError in logging artifacts to MLFlow")
+            except Exception as e:
+                logger.warning(f"Failed to log artifacts: {e}")
 
     @classmethod
     def end_run(cls):
-        mlflow.end_run()
+        if not cls.disable_logging:
+            mlflow.end_run()
+
+    @classmethod
+    def disable(cls):
+        logger.warning("ML Logging is turned off. No parameters, metrics or artifacts will be logged to MLFlow.")
+        cls.disable_logging = True
 
 
 class TensorBoardLogger(BaseMLLogger):
