d85c2af7a18a7bed9f537bae9f83ff23d8959975
Malte Pietsch <malte.pietsch@deepset.ai>
2020-12-18 10:56:46 +0100

    optimize speed of mask_random_words

--- examples/lm_finetuning.py
+++ examples/lm_finetuning.py
@@ -34,7 +34,7 @@ def lm_finetuning():
     evaluate_every = 30
     lang_model = "bert-base-cased"
     do_lower_case = False
-    next_sent_pred = True
+    next_sent_pred = False
     next_sent_pred_style = "bert-style"
 
     # 1.Create a tokenizer
@@ -45,8 +45,8 @@ def lm_finetuning():
     # 2. Create a DataProcessor that handles all the conversion from raw text into a pytorch Dataset
     processor = BertStyleLMProcessor(
         data_dir=Path("../data/lm_finetune_nips"),
-        test_filename=None,
         dev_filename=None,
+        test_filename=None,
         tokenizer=tokenizer,
         max_seq_len=128,
         max_docs=20, # We have set max_docs to 20 to speed up data processing
@@ -62,47 +62,47 @@ def lm_finetuning():
     print(f"Time {toc-tic}")
     # 4. Create an AdaptiveModel
     # a) which consists of a pretrained language model as a basis
-    language_model = LanguageModel.load(lang_model)
-    # b) and *two* prediction heads on top that are suited for our task => Language Model finetuning
-    lm_prediction_head = BertLMHead.load(lang_model)
-    next_sentence_head = NextSentenceHead.load(lang_model)
-
-    model = AdaptiveModel(
-        language_model=language_model,
-        prediction_heads=[lm_prediction_head, next_sentence_head],
-        embeds_dropout_prob=0.1,
-        lm_output_types=["per_token", "per_sequence"],
-        device=device,
-    )
-
-    # 5. Create an optimizer
-    model, optimizer, lr_schedule = initialize_optimizer(
-        model=model,
-        learning_rate=2e-5,
-        device=device,
-        n_batches=len(data_silo.loaders["train"]),
-        n_epochs=n_epochs,
-    )
-
-    # 6. Feed everything to the Trainer, which keeps care of growing our model into powerful plant and evaluates it from time to time
-    trainer = Trainer(
-        model=model,
-        optimizer=optimizer,
-        data_silo=data_silo,
-        epochs=n_epochs,
-        n_gpu=n_gpu,
-        lr_schedule=lr_schedule,
-        evaluate_every=evaluate_every,
-        device=device,
-    )
-
-    # 7. Let it grow! Watch the tracked metrics live on the public mlflow server: https://public-mlflow.deepset.ai
-    trainer.train()
-
-    # 8. Hooray! You have a model. Store it:
-    save_dir = Path("saved_models/bert-english-lm-tutorial")
-    model.save(save_dir)
-    processor.save(save_dir)
+    # language_model = LanguageModel.load(lang_model)
+    # # b) and *two* prediction heads on top that are suited for our task => Language Model finetuning
+    # lm_prediction_head = BertLMHead.load(lang_model)
+    # next_sentence_head = NextSentenceHead.load(lang_model)
+    #
+    # model = AdaptiveModel(
+    #     language_model=language_model,
+    #     prediction_heads=[lm_prediction_head, next_sentence_head],
+    #     embeds_dropout_prob=0.1,
+    #     lm_output_types=["per_token", "per_sequence"],
+    #     device=device,
+    # )
+    #
+    # # 5. Create an optimizer
+    # model, optimizer, lr_schedule = initialize_optimizer(
+    #     model=model,
+    #     learning_rate=2e-5,
+    #     device=device,
+    #     n_batches=len(data_silo.loaders["train"]),
+    #     n_epochs=n_epochs,
+    # )
+    #
+    # # 6. Feed everything to the Trainer, which keeps care of growing our model into powerful plant and evaluates it from time to time
+    # trainer = Trainer(
+    #     model=model,
+    #     optimizer=optimizer,
+    #     data_silo=data_silo,
+    #     epochs=n_epochs,
+    #     n_gpu=n_gpu,
+    #     lr_schedule=lr_schedule,
+    #     evaluate_every=evaluate_every,
+    #     device=device,
+    # )
+    #
+    # # 7. Let it grow! Watch the tracked metrics live on the public mlflow server: https://public-mlflow.deepset.ai
+    # trainer.train()
+    #
+    # # 8. Hooray! You have a model. Store it:
+    # save_dir = Path("saved_models/bert-english-lm-tutorial")
+    # model.save(save_dir)
+    # processor.save(save_dir)
 
 
 if __name__ == "__main__":
