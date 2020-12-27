c408e32168795e94d6a24b2ed98515405c28a847
Malte Pietsch <malte.pietsch@deepset.ai>
2020-12-27 22:08:13 +0100

    fix model version tests

--- test/test_model_versioning.py
+++ test/test_model_versioning.py
@@ -13,20 +13,20 @@ def test_wrong_revision(caplog=None):
 
 def test_revision_v1(caplog=None):
     model = Inferencer.load("deepset/roberta-base-squad2", revision="v1.0", task_type="question_answering")
-    assert torch.equal(torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
+    assert torch.isclose(torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
                 torch.sum(torch.tensor([-21394.6055])))
     del model
 
 def test_revision_v2(caplog=None):
     model = Inferencer.load("deepset/roberta-base-squad2", revision="v2.0", task_type="question_answering")
-    assert torch.equal(torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
+    assert torch.isclose(torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
                        torch.sum(torch.tensor([-21411.4414])))
     del model
 
 def test_revision_default(caplog=None):
     # default model should be the same as v2
     model = Inferencer.load("deepset/roberta-base-squad2", task_type="question_answering")
-    assert torch.equal(
+    assert torch.isclose(
         torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
         torch.sum(torch.tensor([-21411.4414])))
     del model
