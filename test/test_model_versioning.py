6156a2e13d2a46c5e3bc369d55507876e8e92102
Malte Pietsch <malte.pietsch@deepset.ai>
2020-12-27 13:27:46 +0100

    split model versioning tests

--- test/test_model_versioning.py
+++ test/test_model_versioning.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 from farm.infer import Inferencer
 
-def test_model_versioning(caplog=None):
+def test_wrong_revision(caplog=None):
     # We want this load attempt to fail because we specify an invalid revision
     failed_load = None
     try:
@@ -11,13 +11,19 @@ def test_model_versioning(caplog=None):
         pass
     assert not failed_load
 
+def test_revision_v1(caplog=None):
     model_v1 = Inferencer.load("deepset/roberta-base-squad2", revision="v1.0", task_type="question_answering")
-    model_v2 = Inferencer.load("deepset/roberta-base-squad2", revision="v2.0", task_type="question_answering")
-    model_default = Inferencer.load("deepset/roberta-base-squad2", task_type="question_answering")
+    assert torch.equal(torch.sum(model_v1.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
+                torch.sum(torch.tensor([-21394.6055])))
 
-    weights_v1 = model_v1.model.language_model.model.encoder.layer[0].intermediate.dense.weight
-    weights_v2 = model_v2.model.language_model.model.encoder.layer[0].intermediate.dense.weight
-    weights_default = model_default.model.language_model.model.encoder.layer[0].intermediate.dense.weight
+def test_revision_v2(caplog=None):
+    model = Inferencer.load("deepset/roberta-base-squad2", revision="v2.0", task_type="question_answering")
+    assert torch.equal(torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
+                       torch.sum(torch.tensor([-21411.4414])))
 
-    assert torch.equal(weights_default, weights_v2)
-    assert not torch.equal(weights_v1, weights_v2)
+def test_revision_default(caplog=None):
+    # default model should be the same as v2
+    model = Inferencer.load("deepset/roberta-base-squad2", task_type="question_answering")
+    assert torch.equal(
+        torch.sum(model.model.language_model.model.encoder.layer[0].intermediate.dense.weight),
+        torch.sum(torch.tensor([-21411.4414])))
