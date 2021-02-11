Fix call to tokenizers, change statistic calculation for biencoders, add dpr integration test

test/test_dpr.py
@@ -3,6 +3,9 @@ import torch
 import logging
 import numpy as np
 from farm.data_handler.processor import TextSimilarityProcessor
+from farm.data_handler.data_silo import DataSilo
+from farm.train import Trainer
+from farm.modeling.optimization import initialize_optimizer
 from farm.modeling.biadaptive_model import BiAdaptiveModel
 from farm.modeling.language_model import LanguageModel, DPRContextEncoder, DPRQuestionEncoder
 from farm.modeling.prediction_head import TextSimilarityHead
@@ -439,7 +442,91 @@ def test_dpr_save_load():
     dataset2, tensor_names, _ = loadedprocessor.dataset_from_dicts(dicts=[d], return_baskets=False)
     assert np.array_equal(dataset.tensors[0],dataset2.tensors[0])
 
+
+def test_dpr_training():
+    batch_size = 1
+    n_epochs = 1
+    distributed = False  # enable for multi GPU training via DDP
+    evaluate_every = 1
+    question_lang_model = "microsoft/MiniLM-L12-H384-uncased"
+    passage_lang_model = "microsoft/MiniLM-L12-H384-uncased"
+    do_lower_case = True
+    use_fast = True
+    similarity_function = "dot_product"
+
+
+
+    device, n_gpu = initialize_device_settings(use_cuda=False)
+
+    query_tokenizer = Tokenizer.load(pretrained_model_name_or_path=question_lang_model,
+                                     do_lower_case=do_lower_case, use_fast=use_fast)
+    passage_tokenizer = Tokenizer.load(pretrained_model_name_or_path=passage_lang_model,
+                                       do_lower_case=do_lower_case, use_fast=use_fast)
+    label_list = ["hard_negative", "positive"]
+
+    processor = TextSimilarityProcessor(query_tokenizer=query_tokenizer,
+                                        passage_tokenizer=passage_tokenizer,
+                                        max_seq_len_query=10,
+                                        max_seq_len_passage=10,
+                                        label_list=label_list,
+                                        metric="text_similarity_metric",
+                                        data_dir="samples/dpr/",
+                                        train_filename="sample.json",
+                                        dev_filename="sample.json",
+                                        test_filename=None,
+                                        embed_title=True,
+                                        num_hard_negatives=1,
+                                        dev_split=0,
+                                        max_samples=2)
+
+    data_silo = DataSilo(processor=processor, batch_size=batch_size, distributed=False)
+
+    question_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased",
+                                                 language_model_class="DPRQuestionEncoder")
+    passage_language_model = LanguageModel.load(pretrained_model_name_or_path="bert-base-uncased",
+                                                language_model_class="DPRContextEncoder")
+
+    prediction_head = TextSimilarityHead(similarity_function=similarity_function)
+
+    model = BiAdaptiveModel(
+        language_model1=question_language_model,
+        language_model2=passage_language_model,
+        prediction_heads=[prediction_head],
+        embeds_dropout_prob=0.1,
+        lm1_output_types=["per_sequence"],
+        lm2_output_types=["per_sequence"],
+        device=device,
+    )
+
+    model, optimizer, lr_schedule = initialize_optimizer(
+        model=model,
+        learning_rate=1e-5,
+        optimizer_opts={"name": "TransformersAdamW", "correct_bias": True, "weight_decay": 0.0, \
+                        "eps": 1e-08},
+        schedule_opts={"name": "LinearWarmup", "num_warmup_steps": 100},
+        n_batches=len(data_silo.loaders["train"]),
+        n_epochs=n_epochs,
+        grad_acc_steps=1,
+        device=device,
+        distributed=distributed
+    )
+
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+        data_silo=data_silo,
+        epochs=n_epochs,
+        n_gpu=n_gpu,
+        lr_schedule=lr_schedule,
+        evaluate_every=evaluate_every,
+        device=device,
+    )
+
+    trainer.train()
+
+
+
 if __name__=="__main__":
-    test_dpr_problematic()
+    test_dpr_training()
     #test_dpr_context_only()
     # test_dpr_modules()