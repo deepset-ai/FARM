53f402aeda12b1ec69d9851ed3b81a969f09e97d
Julian Risch <julian.risch@deepset.ai>
2021-05-18 11:02:26 +0200

    Removed whitespace from test case question

--- test/test_dpr.py
+++ test/test_dpr.py
@@ -445,7 +445,7 @@ def test_dpr_processor_save_load():
 
 
 def test_dpr_processor_save_load_non_bert_tokenizer():
-    d = {'query': "Comment s'appelle le portail open data du gouvernement ?",
+    d = {'query': "Comment s'appelle le portail open data du gouvernement?",
          'passages': [
              {'title': 'Etalab',
               'text': "Etalab est une administration publique française qui fait notamment office de Chief Data Officer de l'État et coordonne la conception et la mise en œuvre de sa stratégie dans le domaine de la donnée (ouverture et partage des données publiques ou open data, exploitation des données et intelligence artificielle...). Ainsi, Etalab développe et maintient le portail des données ouvertes du gouvernement français data.gouv.fr. Etalab promeut également une plus grande ouverture l'administration sur la société (gouvernement ouvert) : transparence de l'action publique, innovation ouverte, participation citoyenne... elle promeut l’innovation, l’expérimentation, les méthodes de travail ouvertes, agiles et itératives, ainsi que les synergies avec la société civile pour décloisonner l’administration et favoriser l’adoption des meilleures pratiques professionnelles dans le domaine du numérique. À ce titre elle étudie notamment l’opportunité de recourir à des technologies en voie de maturation issues du monde de la recherche. Cette entité chargée de l'innovation au sein de l'administration doit contribuer à l'amélioration du service public grâce au numérique. Elle est rattachée à la Direction interministérielle du numérique, dont les missions et l’organisation ont été fixées par le décret du 30 octobre 2019.  Dirigé par Laure Lucchesi depuis 2016, elle rassemble une équipe pluridisciplinaire d'une trentaine de personnes.",
