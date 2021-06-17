501927e081d1513633aafd9f195b36b186258518
Timo Moeller <timo.moeller@deepset.ai>
2021-06-17 18:02:30 +0200

    refactor

--- farm/evaluation/semantic_answer_similarity_evaluation.py
+++ farm/evaluation/semantic_answer_similarity_evaluation.py
@@ -9,6 +9,7 @@ def semantic_answer_similarity(result, sts_model_path_or_string="paraphrase-xlm-
     Computes BERT based similarity of prediction to gold labels.
     Returns per QA pair a) the similarity of the most likely prediction to all available gold labels
                         b) the highest similarity of all predictions to gold labels
+                        c) the original input populated with semantic answer scores per prediction
 
     :param result: Output of QAinferencer.inference_from_file when setting return_json=False. Contains predictions
                    and annotation information.
@@ -20,26 +21,28 @@ def semantic_answer_similarity(result, sts_model_path_or_string="paraphrase-xlm-
     :type str
     :param debug: Collect additional info for interesting cases for debugging purposes (default False).
                   Interesting cases are predictions that do not overlap with any of the gold labels (topnNAcc == 0).
+
     :type debug: bool
 
     Returns the average of the semantically evaluated best, and Top N predictions as well as the List of
     QApreds (result) with the semantic eval score added to each prediction in its "meta" field.
-    :return best_pred_similarity, all_preds_highest_similarity, result, Optional[List[Dict]]
+    :return best_pred_similarity, all_preds_highest_similarity, result, debug_info
     """
     text_preds, text_labels = _extract_QA_result(result)
     assert len(text_preds) == len(text_labels)
 
     model = SentenceTransformer(sts_model_path_or_string)
-    # flatten predictions and labels into one list to batch the encoding
+    # Flatten predictions and labels into one list to improve encoding speed through proper batching
     lengths = []
     all_texts = []
     for p, l in zip(text_preds, text_labels):
+        # TODO potentially exclude (near) exact matches from computations
         all_texts.extend(p)
         all_texts.extend(l)
         lengths.append((len(p), len(l)))
     embeddings = model.encode(all_texts)
 
-    # compute similarities
+    # Compute similarities
     top1_sim = []
     topn_sim = []
     debug_info = []
@@ -56,30 +59,16 @@ def semantic_answer_similarity(result, sts_model_path_or_string="paraphrase-xlm-
         for j,p in enumerate(result[i].prediction):
             p.meta = {"semantic_answer_score":np.max(sims[j, :])}
 
+        # Add debug information
         if debug:
-            # exclude no answer labels
+            # Exclude no answer labels from debug info
             if len(result[i].ground_truth_answer) == 0:
                 pass
             else:
-                current_preds = [[result[i].prediction]]
-                current_labels = [
-                    [(x["answer_start"], x["answer_start"] + len(x["text"])) for x in result[i].ground_truth_answer]]
-                if top_n_accuracy(preds=current_preds, labels=current_labels) == 0:
-                    current_info = {}
-                    current_info["question"] = result[i].question
-                    current_info["top1_sim"] = np.max(sims[0, :])
-                    current_info["top1_label"] = result[i].ground_truth_answer[np.argmax(sims[0, :])]["text"]
-                    current_info["top1_pred"] = result[i].prediction[0].answer
+                current_info = _add_debug_info(result=result, i=i, sims=sims)
+                debug_info.append(current_info)
 
-                    idx = np.unravel_index(sims.argmax(), sims.shape)
-                    current_info["topn_sim"] = np.max(sims)
-                    current_info["topn_label"] = result[i].ground_truth_answer[idx[1]]["text"]
-                    current_info["topn_pred"] = result[i].prediction[idx[0]].answer
-                    debug_info.append(current_info)
-    if debug:
-        return np.mean(top1_sim), np.mean(topn_sim), result, debug_info
-    else:
-        return np.mean(top1_sim), np.mean(topn_sim), result
+    return np.mean(top1_sim), np.mean(topn_sim), result, debug_info
 
 # helper fct to extract labels and predictions in text form
 def _extract_QA_result(result):
@@ -100,3 +89,21 @@ def _extract_QA_result(result):
         text_preds.append(current_preds)
 
     return text_preds, text_labels
+
+# helper fct to add debug info
+def _add_debug_info(result,i,sims):
+    current_preds = [[result[i].prediction]]
+    current_labels = [
+        [(x["answer_start"], x["answer_start"] + len(x["text"])) for x in result[i].ground_truth_answer]]
+    if top_n_accuracy(preds=current_preds, labels=current_labels) == 0:
+        current_info = {}
+        current_info["question"] = result[i].question
+        current_info["top1_sim"] = np.max(sims[0, :])
+        current_info["top1_label"] = result[i].ground_truth_answer[np.argmax(sims[0, :])]["text"]
+        current_info["top1_pred"] = result[i].prediction[0].answer
+
+        idx = np.unravel_index(sims.argmax(), sims.shape)
+        current_info["topn_sim"] = np.max(sims)
+        current_info["topn_label"] = result[i].ground_truth_answer[idx[1]]["text"]
+        current_info["topn_pred"] = result[i].prediction[idx[0]].answer
+    return current_info
